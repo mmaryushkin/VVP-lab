284c5af65a5da1086d1de83f795099e8dbc8f5c2
Matthew Maryushkin <shspankratov@gmail.com>
2020-09-20 12:41:30 +0300

    Сумма, разность, произведение и частное квадратов двух чисел

---
+++ Задание к теме 4/4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus"); // подключение русского языка
+    float a, b;
+
+    // считываем числа a и b
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    cout << "Сумма квадратов равна: " << a * a + b * b << "\n";
+    cout << "Разность квадратов равна: " << a * a - b * b << "\n";
+    cout << "Произведение квадратов равно: " << a * a * b * b << "\n";
+    cout << "Частное квадратов равно: " << a * a / b * b;
+
+    return 0;
+}
