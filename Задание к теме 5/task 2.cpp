f67228fd149948a5da2a309257f7f879d5ddc684
Matthew Maryushkin <shspankratov@gmail.com>
2020-09-27 21:59:02 +0300

    Переосмысление условия 2-ого задания

--- Задание к теме 5/task 2.cpp
+++ Задание к теме 5/task 2.cpp
@@ -2,37 +2,16 @@
 
 using namespace std;
 
-float pointsDistance(float x1, float y1, float x2, float y2)
-{
-    float d;
-    
-    float s1 = pow((x2 - x1), 2); // первое слагаемое
-    float s2 = pow((y2 - y1), 2); // второе слагаемое
-    d = sqrt(s1 + s2);
-
-    return d;
-}
-
-
 int main()
 {
     setlocale(LC_ALL, "rus");
-    float Ax, Ay, Bx, By, Cx, Cy;
+    float A, B, C, AC, BC;
+    cout << "Введите расположение на числовой оси точек А В С: ";
+    cin >> A >> B >> C;
+    // длины отрезков АС и ВС
+    AC = abs(A - C);
+    BC = abs(B - C);
 
-    // считываем координаты точек
-    cout << "Введите координаты точки А: ";
-    cin >> Ax >> Ay;
-
-    cout << "Введите координаты точки В: ";
-    cin >> Bx >> By;
-
-    cout << "Введите координаты точки С: ";
-    cin >> Cx >> Cy;
-
-    // вычисление длин отрезков АС и ВС
-    float AC = pointsDistance(Ax, Ay, Cx, Cy);
-    float BC = pointsDistance(Bx, By, Cx, Cy);
-
-    cout << "Сумма отрезков АС и ВС равна: " << AC + BC;
+    cout << "\n" << "Сумма отрезков АС и ВС равна: " << AC + BC << "\n";
     return 0;
 }
