9fecefbcc35ccd5da038f75c2bdf5daaa7431347
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-03 16:31:13 +0300

    число в 15 степени в 5 умножений

---
+++ Задание к теме 6/task 7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите А: ");
+        int A = int.Parse(Console.ReadLine());
+
+        int b = A*A; // A^2
+        int c = b * A; // A^3
+        b *= c; // A^5
+        c = b;
+        b *= b; // A^10
+        b *= c; // A^15
+        
+        Console.WriteLine($"A^8 = {b}");
+    }
+}
