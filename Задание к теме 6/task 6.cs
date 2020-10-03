c0e4a315bb80ae65e4d772bd29ecb52052927a8e
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-03 16:20:36 +0300

    A^8 в три умножения

--- Задание к теме 6/task 6.cs
+++ Задание к теме 6/task 6.cs
@@ -8,7 +8,7 @@ class Program
         int A = int.Parse(Console.ReadLine());
         
         int b = A*A; // A^2
-        b *= A*A; // A^4
+        b *= b; // A^4
         b *= b; // A^8
 
         Console.WriteLine($"A^8 = {b}");
