dad720b37aad17be576cb0b3e93c5115cc6a1a34
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-03 16:09:12 +0300

    число в 8-ой степени

---
+++ Задание к теме 6/task 6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите А: ");
+        int A = int.Parse(Console.ReadLine());
+        
+        int b = A*A; // A^2
+        b *= A*A; // A^4
+        b *= b; // A^8
+
+        Console.WriteLine($"A^8 = {b}");
+    }
+}
