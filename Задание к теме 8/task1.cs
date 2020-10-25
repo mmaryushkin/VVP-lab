dc821f44c87543e3ab042f2739915a5192a5394f
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-25 14:55:05 +0300

    Байты в килобайты

---
+++ Задание к теме 8/task1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Размер файла в байтах: ");
+        int byteSize = int.Parse(Console.ReadLine());
+
+        int kylobSize = byteSize / 8;
+        Console.WriteLine($"\nРазмер файла в килобайтах: {kylobSize}"); 
+    }
+}
