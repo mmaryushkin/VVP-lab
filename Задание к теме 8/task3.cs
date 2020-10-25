c7704d55214b07731581cde382f64bebab47544c
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-25 15:16:23 +0300

    Длина незанятой части отрезка

---
+++ Задание к теме 8/task3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Длина отрезка A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Длина отрезка В: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int n = a % b;
+        Console.WriteLine($"\nДлина незанятой части отрезка A: {n}");
+    }
+}
