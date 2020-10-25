4cc707c70e9534fccc2e403b31b8c6e72b07141c
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-25 15:28:08 +0300

    Перенос цифры слева направо

---
+++ Задание к теме 8/task5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите трехзначное число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write(a % 100);
+        Console.Write(a / 100);
+    }
+}
