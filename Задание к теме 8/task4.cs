03220377399c2d1d08ce5c93bbf29f8426a868ae
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-25 15:24:14 +0300

    Двузначное число наоборот

---
+++ Задание к теме 8/task4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите двузначное число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        while (a > 0)
+        {
+            Console.Write(a % 10);
+            a /= 10;
+        }
+    }
+}
