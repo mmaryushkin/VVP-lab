086a5de8cda0e97d92a541578f5f5fa52ab5f9ed
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-26 22:28:50 +0300

    День недели

---
+++ Задание к теме 9/task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] days = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
+
+        int k = int.Parse(Console.ReadLine());
+        Console.WriteLine(days[k % 7]);
+    }
+}
