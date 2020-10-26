d351014bac7e94dca582efbc03b19770251c37a4
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-26 22:14:03 +0300

    Количество секунд, прошедших с начала последней минуты

---
+++ Задание к теме 9/task1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(n % 60);
+    }
+}
