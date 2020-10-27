bb7a8f140026a35dc4a94976260024b3fe21354a
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 12:37:09 +0300

    Номер дня недели с номером К, 1 января - N

---
+++ Задание к теме 9/task3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] days = { 7, 1, 2, 3, 4, 5, 6 };
+        
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine()); // 1 января
+
+        Console.Write("K = ");
+        int k = int.Parse(Console.ReadLine()); // день года
+
+        Console.WriteLine(days[(k + n-1) % 7]);
+    }
+}
