1910c27acf684d94a503506fca71a3b9aada83fd
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 15:34:10 +0300

    Двойное неравенство

---
+++ Задание к теме 10/task2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("C = ");
+        int c = int.Parse(Console.ReadLine());
+
+        bool inq = (b > a && b < c);
+        Console.WriteLine($"A < B < C : {inq}");
+    }
+}
