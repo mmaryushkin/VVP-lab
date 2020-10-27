d8e0c8c0e84ed1fca3d62c1b755b4962d902f8a6
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 15:43:39 +0300

    Четное двузначное число

---
+++ Задание к теме 10/task3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool statement = (n > 9 && n < 100 && n % 2 == 0);
+
+        Console.WriteLine($"Число двузначное и четное: {statement}"); 
+    }
+}
