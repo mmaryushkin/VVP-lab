02d382dc3b7941dcdde6efe9d6907d7ce76576ef
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 16:54:04 +0300

    Возрастающая/убывающая последовательность цифр

---
+++ Задание к теме 10/task4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int e = n % 10; // разряд единиц
+        int d = n / 10 % 10; // разряд десятков
+        int c = n / 100; // разряд сотен
+
+        if (e < d && d < c)
+        {
+            Console.WriteLine("Цифры числа образуют убывающую последовательность");
+        }
+
+        else if (c < d && d < e)
+        {
+            Console.WriteLine("Цивры образуют возрастающую последовательность");
+        }
+    }
+}
