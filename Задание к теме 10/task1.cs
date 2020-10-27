9d3c518c4d598f2c8baf10c92b20be4ea5cab0d0
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 15:20:11 +0300

    Проверка истинности высказывания

---
+++ Задание к теме 10/task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+
+        bool inequality = (a > 2 && b <= 3);
+        Console.WriteLine($"Высказывание «Справедливы неравенства A > 2 и B ≤ 3»: {inequality}");
+    }
+}
