4109dfbe15ea0874375fc76d468716d21074bbe5
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 17:31:11 +0300

    Прямоугольный треугольник

---
+++ Задание к теме 10/task6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine()); // 1-я сторона
+
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine()); // 2-я сторона
+
+        Console.Write("c = ");
+        int c = int.Parse(Console.ReadLine()); // 3-я сторона
+
+        bool triangle = (a*a + b*b == c*c);
+
+        Console.WriteLine($"Треугольник прямоугольный: {triangle}");
+    }
+}
