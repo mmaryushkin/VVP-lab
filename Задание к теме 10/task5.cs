4b3d35ca63cdf30542b0bae61a39fbfb853d6aad
Matthew Maryushkin <shspankratov@gmail.com>
2020-10-27 17:18:22 +0300

    Палиндром

---
+++ Задание к теме 10/task5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool palindrom = (n == Reverse(n));
+
+        Console.WriteLine($"Число одинаково читается слева направо и справа налево: {palindrom}");
+    }
+
+    static double Reverse(int n)
+    {
+        int r = 0;
+        
+        while (n > 0)
+        {
+            r = r * 10 + n % 10;
+            n /= 10;
+        }
+
+        return r;
+    }
+}
