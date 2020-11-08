9c876ba3dc36dba70fff6323664f70e26bd22152
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-08 18:55:17 +0300

    Сумма двух наибольших чисел из трех

---
+++ задание к теме 11/task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+    
+        int maximum = Max(c, Max(a, b)); // максимальное число из трех
+
+        if (maximum == a)
+            Console.WriteLine(a + Max(b, c)); // сумма а и максимального из b и с
+
+        if (maximum == b)
+            Console.WriteLine(b + Max(a, c)); // сумма b и максимального из a и с
+        
+        if (maximum == c)
+            Console.WriteLine(c + Max(a, b)); // сумма с и максимального из a и b
+    }    
+
+    static int Max(int a, int b)
+    {
+        if (a > b)
+            return a;
+
+        return b;
+    }
+}
