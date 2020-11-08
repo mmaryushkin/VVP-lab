87afadda639ee3d7d697bdeeedf6186e7cc30873
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-08 22:13:49 +0300

    Характеристика трехзначных и двухзначных чисел

---
+++ задание к теме 11/task6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        if (a >= 10 && a <= 99 && a % 2 == 0)
+            Console.WriteLine("Четное двузначное число");
+
+        if (a >= 10 && a <= 99 && a % 2 != 0)
+            Console.WriteLine("Нечетное двузначное число");
+
+        if (a >= 100 && a <= 999 && a % 2 == 0)
+            Console.WriteLine("Четное трехзначное число");
+
+        if (a >= 100 && a <= 999 && a % 2 != 0)
+            Console.WriteLine("Нечетное трехзначное число");
+    }
+}
