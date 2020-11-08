2a19de46ef95715d0b2f95a23786af37d6bfa0e2
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-08 20:52:10 +0300

    определение четверти точки

---
+++ задание к теме 11/task4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Координаты точки: ");
+        string[] arr = Console.ReadLine().Split();
+        int x = int.Parse(arr[0]);
+        int y = int.Parse(arr[1]);
+
+        if (x > 0 && y > 0)
+            Console.WriteLine("1-я четверть");
+
+        if (x < 0 && y > 0)
+            Console.WriteLine("2-я четверть");
+
+        if (x < 0 && y < 0)
+            Console.WriteLine("3-я четверть");
+
+        if (x > 0 && y < 0)
+            Console.WriteLine("4-я четверть");
+    }
+}
