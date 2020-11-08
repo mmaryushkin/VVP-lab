d6a71cfecf047b8796e3fc9dc96c3779f8296bac
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-08 21:43:24 +0300

    Характеристика числа

---
+++ задание к теме 11/task5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int a = int.Parse(Console.ReadLine());
+        
+        if (a == 0)
+            Console.WriteLine("Нулевое число");
+
+        if (a > 0 && a % 2 == 0)
+            Console.WriteLine("Положительное четное число");
+
+        if (a > 0 && a % 2 != 0)
+            Console.WriteLine("Положительное нечетное число");
+
+        if (a < 0 && a % 2 == 0)
+            Console.WriteLine("Отрицательное четное число");
+
+        if (a < 0 && a % 2 != 0)
+            Console.WriteLine("Отрицательное нечетное число");
+    }
+}
