f471d339e1a50a1e770172055789aae0f4b0eba4
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-08 17:47:38 +0300

    присваивание большего значения

---
+++ задание к теме 11/task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        if (a != b)
+        {
+            if (a > b)
+                b = a;
+            
+            else
+                a = b;
+        }
+
+        Console.WriteLine($"{a} {b}");
+    }    
+}
