e19cf1ad92aad0d12e40ac5cc085846f260e4c3c
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-09 13:28:07 +0300

    направление робота

---
+++ задание к теме 12/task2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] directions = { "С", "В", "Ю", "З" };
+
+        Console.Write("Введите направление робота: ");
+        int c = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите команду: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(directions[(c + n) % 4]);
+    }
+}
