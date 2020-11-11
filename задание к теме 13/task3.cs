690bab40cc4b679f917178ded15c6957c6435de5
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-11 17:01:28 +0300

    Квадрат числа

---
+++ задание к теме 13/task3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int n2 = 0;
+        for (int i = n; i > 0; i--)
+            n2 += 2 * i - 1;
+        Console.WriteLine(n2);
+    }
+}
