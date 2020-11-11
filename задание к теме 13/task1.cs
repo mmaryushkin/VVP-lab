ea0176a1b76053736968f5c30f5c013bc313f039
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-11 16:45:02 +0300

    Десятые части целого

---
+++ задание к теме 13/task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (float i = 0.1f; i < 1; i += 0.1f)
+        {
+            Console.WriteLine(n * i);
+        }
+    }
+}
