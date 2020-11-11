be79a743a5e906cb1ff80a1a456f06106ea79aa7
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-11 16:54:36 +0300

    произведение 1.1 · 1.2 · 1.3 · . . . (N сомножителей).

---
+++ задание к теме 13/task2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double p = 1.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            p *= 1 + (double)i / 10;
+        }
+
+        Console.WriteLine(p);
+    }
+}
