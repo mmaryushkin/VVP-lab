6f885fbf2fbab5f03b366dac8ce4e2e59091b0a8
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 12:12:04 +0300

    наименьшее из целых чисел K, для которых сумма 1 + 2 + . . . + K будет больше или равна N

---
+++ задание к теме 14/task3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int s = 1;
+        int k = 2;
+        while (s + k < n)
+        {
+            s += k;
+            k++;
+        }
+        Console.WriteLine(k);
+    }
+}
