5f8fdfa218c863597446ffa99df7685ba9516d71
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 11:58:43 +0300

    Незанятая часть отрезка

---
+++ задание к теме 14/task2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+
+        while (a >= b*2)
+            b += b;
+
+        Console.WriteLine("Незанятая часть отрезка А: " + (a - b));
+    }
+}
