45bb6cd38c2cfed02b17b5f4e8b4d5370fb60555
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 12:25:56 +0300

    вклад в банке

---
+++ задание к теме 14/task4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float v = 1000f; // начальный вклад
+        float p = float.Parse(Console.ReadLine()); // проценты
+        int k = 0; // счетчик месяцев
+        while (v <= 1100)
+        {
+            v += v * p / 100;
+            k++;
+        }
+        Console.WriteLine("Количество месяцев " + k);
+        Console.WriteLine("Размер вклада " + v);
+    }
+}
