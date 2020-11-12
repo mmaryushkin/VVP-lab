5bd6deb18058040303b5fd12ffa0ed15618c8f77
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 12:30:10 +0300

    алгоритм Евклида

---
+++ задание к теме 14/task5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("a = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("b = ");
+        int b = int.Parse(Console.ReadLine());
+
+        while (a != b)
+        {
+            if (a > b)
+                a -= b;
+
+            if (b > a)
+                b -= a;
+        }
+
+        Console.WriteLine(a);
+    }
+}
