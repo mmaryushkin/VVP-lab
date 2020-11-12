ea62399a44e403f1889e67ff75af59272a89e7a7
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 11:49:57 +0300

    Числа от А до В

---
+++ задание к теме 14/task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+
+        for (int i = a; i <= b; i++)
+        {
+            for (int j = 0; j < i; j++)
+                Console.Write(i + " ");
+        }
+    }
+}
