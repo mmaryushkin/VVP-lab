a57fa1123dfd43237079ea890506d775f19494a8
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-12 13:09:52 +0300

    Порядковый номер числа фиббоначи

---
+++ задание к теме 14/task6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int c = 1;
+        int fib1 = 1;
+        int fib2 = 1;
+        int fib = 0;
+        for (int i = 1; i < n; i++)
+        {
+            fib = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fib;
+            c++;
+        }
+        Console.WriteLine(c);
+    }
+}
