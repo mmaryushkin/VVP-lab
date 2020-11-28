e16c44ad5e04778af56752276962ce5f1d88cbbc
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 00:03:17 +0300

    знак вещественного числа

---
+++ Задание к теме 15/task2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(Sign(x));
+    }
+
+
+    static int Sign(double x)
+    {
+        if (x < 0)
+            return -1;
+        
+        if (x == 0)
+            return 0;
+        
+        return 1;
+    }
+}
