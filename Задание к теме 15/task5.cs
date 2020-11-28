823235b617c714112ccd735c491128f5b350abcb
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 01:25:24 +0300

    Двойной факториал

---
+++ Задание к теме 15/task5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Fact2(n));
+    }
+
+
+    static float Fact2(int n)
+    {
+        float f = 1f;
+        for (int i = 1; i <= n; i += 2)
+            f *= i;
+        return f;
+    }      
+}
