d9707da5901d32f1ef910293f4e766a530542bc2
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 01:18:38 +0300

    Определение четверти точки

---
+++ Задание к теме 15/task4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Координаты точки: ");
+            string[] coordinates = Console.ReadLine().Split();
+            float x = float.Parse(coordinates[0]);
+            float y = float.Parse(coordinates[1]);
+            Console.WriteLine(Quarter(x, y));
+        }
+    }
+
+
+    static int Quarter(float x, float y)
+    {
+        if (x > 0 && y > 0)
+            return 1;
+
+        if (x < 0 && y > 0)
+            return 2;
+
+        if (x < 0 && y < 0)
+            return 3;
+
+        if (x > 0 && y < 0)
+            return 4;
+
+        return 0;
+    }
+}
