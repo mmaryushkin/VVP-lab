c79d8dccfe1567a318726ee58d33d1e7a5829a5b
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-28 23:23:58 +0300

    Число в третьей степени

---
+++ Задание к теме 15/task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float b = 0f; float a;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Введите число: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine(PowerA3(a, b));
+        }
+    }
+
+
+    static float PowerA3(float a, float b)
+    {
+        b = (float) Math.Pow(a, 3);
+        return b;
+    }
+}
