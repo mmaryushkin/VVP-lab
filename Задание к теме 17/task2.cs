b80a67e9754689dfc2a47b7a4188238b77011b4c
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 16:53:11 +0300

    арифметическая прогрессия

---
+++ Задание к теме 17/task2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Progression(arr));
+    }
+    
+
+    static int Progression(int[] arr)
+    {
+        int d = arr[1] - arr[0];
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] != d)
+                return 0;
+        }
+        return d;
+    }
+}
