e1846955c4a9f9884b3e337ebbbb19ba561e0398
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 16:59:25 +0300

    минимальный элемент с четным номером

---
+++ Задание к теме 17/task3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        int max = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        int min = max;
+        for (int i = 2; i < n; i += 2)
+        {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        Console.WriteLine(min);
+    }
+}
