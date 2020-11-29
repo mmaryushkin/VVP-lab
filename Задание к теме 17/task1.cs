8f2f3289aded41e51d6b0ba2759a270dbd7ab438
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 16:39:57 +0300

    среднее арифметическое

---
+++ Задание к теме 17/task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = i + 1;
+
+
+        Console.Write("K = ");
+        int k = int.Parse(Console.ReadLine());
+
+        Console.Write("L = ");
+        int l = int.Parse(Console.ReadLine());
+
+        float s = 0;
+        for (int i = k; i < l; i++)
+            s += arr[i];
+
+        Console.WriteLine(s / (float)(l - k));
+    }   
+}
