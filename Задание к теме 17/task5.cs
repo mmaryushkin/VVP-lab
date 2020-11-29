3822755a3d72eb16a60783aa3a630c9c1ff729c3
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 17:11:05 +0300

    равные элементы массива

---
+++ Задание к теме 17/task5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int k = 0; k < n; k++)
+        {
+            arr[k] = int.Parse(Console.ReadLine());
+        }
+
+        int i = 0;
+        int j = n - 1;
+        while (i != j)
+        {
+            if (arr[i] == arr[j])
+                Console.WriteLine($"{i}, {j}");
+            i++;
+            j--;
+        }
+    }
+}
