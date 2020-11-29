7187ff73af0fc947c9d4fdedc57dd49da4e68323
Matthew Maryushkin <shspankratov@gmail.com>
2020-11-29 17:04:07 +0300

    Индекс локального максимума

---
+++ Задание к теме 17/task4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxIndex = -1;
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                maxIndex = i;
+        }
+
+        Console.WriteLine(maxIndex);
+    }
+}
